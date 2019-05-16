1147c37127b040306b04c250098d4715c6da6861
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-16 06:07:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,9 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    if BRANDS.!include?(brand)
+    if BRANDS.include?(brand)
+      nil
+    else
       BRANDS << brand
     end
   end
